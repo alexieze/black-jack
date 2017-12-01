47e28945d25c6b6c63996e8a5f7d0368ce313198
Albert Khasanov <albert@gestiv.ru>
2017-12-01 11:14:01 +0600

    Black Jack v 0.1.0

--- diller.rb
+++ diller.rb
@@ -8,12 +8,10 @@ class Diller < User
   def move
     puts hand.sum_points
     if hand.sum_points == 21
-      self.open_cards = true
-    elsif hand.sum_points >= 19 && hand.isset_ace && !self.open_cards?
-      hand.take_card
-    elsif hand.sum_points >= 15
-      self.skip_step = true unless self.skipped_step? && hand.sum_points > 10
-    elsif hand.sum_points < 15
+      self.open_cards = true if !self.open_cards?
+    elsif hand.sum_points >= 18 && !skipped_step?
+      self.skip_step = true
+    else
       hand.take_card
     end
   end
--- interface.rb
+++ interface.rb
@@ -2,23 +2,21 @@ require_relative 'game'
 
 class Interface
 
-  attr_reader :game
+  attr_reader :game, :username
 
   def initialize
     puts 'Введите имя игрока:'
-    username = gets.chomp.to_s
-    @game = Game.new(username)
-
+    @username = gets.chomp.to_s
   end
 
 
   def game_init
     loop do
+      @game = Game.new(username)
       game.giving_away_money
       game.make_bet
       game.giving_away_cards
       round
-      break
       if game.over?
         if !game_new
           break
@@ -32,17 +30,17 @@ class Interface
     loop do
       see_cards(game.player)
       game.player.move(command)
-      #see_cards(game.player)
+      see_cards(game.player)
       break if game.player.open_cards? || (game.player.hand.full? && i > 1)
       game.diller.move
       break
       break if game.diller.open_cards? || game.diller.hand.full?
       i = i + 1
     end
-    #puts 'Результат игрока ====================='
-    #see_cards(game.player)
-    #puts 'Результат диллера ====================='
-    #see_cards(game.diller)
+    puts 'Результат игрока ====================='
+    see_cards(game.player)
+    puts 'Результат диллера ====================='
+    see_cards(game.diller)
     puts game.result
   end
 
@@ -52,7 +50,7 @@ class Interface
 
   def game_new
     puts 'Ееще играть yes/no'
-    input = gets.chomp.to_s
+    gets.chomp.to_s
   end
 
   def command
